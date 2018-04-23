c407c8d8058198d7ca51563f5049bf96e6c779d9
Jacson Santos <jacsos-@hotmail.com>
2018-04-23 18:44:34 -0300

    Criando classe CacheManager
    Criando classe CacheManager

---
+++ CacheManager.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jacson
+ * Date: 23/04/18
+ * Time: 15:08
+ */
+
+namespace App\Src\Helps;
+
+use Illuminate\Cache\Repository;
+
+class CacheManager
+{
+    protected $cache;
+
+    private $enable;
+
+    private $minutes;
+
+    private $data = [];
+
+    public function __construct(Repository $cache, $minutes = 5)
+    {
+        $this->enable = env('CACHE_EN');
+        $this->cache = $cache;
+        $this->minutes = $minutes;
+    }
+
+    public function __set($key,$value)
+    {
+        if ($this->enable && !$this->cache->has($key)) {
+            $this->cache->put($key,$value,$this->minutes);
+        } else {
+            $this->data[$key] = $value;
+        }
+    }
+    public function __get($key)
+    {
+        if ($this->enable && $this->cache->has($key)) {
+            return $this->cache->get($key);
+        }
+
+        if (array_key_exists($key, $this->data)) {
+            return $this->data[$key];
+        }
+
+        return null;
+    }
+    public function has($key)
+    {
+        if ($this->enable && $this->cache->has($key)) {
+            return true;
+        }
+
+        if (array_key_exists($key, $this->data)) {
+            return true;
+        }
+
+        return false;
+    }
+}
